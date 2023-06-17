3ac40810c2a73487bfbef72513357d41a3510a9c
Himanshu Parti <himanshuparti@gmail.com>
2023-06-17 14:51:22 +0530

    adding train python file and ipynb files
    Cannot add model as it is over 100 mbs and github doesn't allow it

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,76 @@
 import sys
 
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+import nltk
+nltk.download(['punkt', 'wordnet', 'omw-1.4'])
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+import pickle
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('Disaster_Data', engine)
+    # There is 2 in the related column so we will replace it with 1
+    df['related'] = df['related'].replace(2,1)
+    # splitting into target and predicting variables
+    X = df.message
+    y = df[df.columns[4:]]
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regular = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    urls = re.findall(url_regular, text)
+    for url in urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    token = word_tokenize(text)
+    lemma = WordNetLemmatizer()
+    
+    token_list = []
+    for tok in token:
+        clean_tok = lemma.lemmatize(tok).lower().strip()
+        token_list.append(clean_tok)
+    
+    return token_list
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+    ('vec_count', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('mo_clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+        'mo_clf__estimator__n_estimators': [40],
+        'mo_clf__estimator__min_samples_split': [3],
+    
+    }
+    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=2, verbose=2, cv=3)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_prediction = model.predict(X_test)
+    class_report = classification_report(Y_test, y_prediction, target_names=category_names)
+    print(class_report)
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
